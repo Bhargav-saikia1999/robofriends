df863cf977d57b69e070f9559557a7df228af293
bhargav-saikia <bhargav.saikia@iitg.ac.in>
2020-08-26 11:27:57 +0530

    Updates

---
+++ precache-manifest.8ba9d5b6ec6018499f01b4db8e69a1f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd7330ef429aff51aea7fb84c8c5949",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "219a2a2b7477fd42e492",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3211c322366f40e90431",
+    "url": "/robofriends/static/css/main.b1fb1cc3.chunk.css"
+  },
+  {
+    "revision": "219a2a2b7477fd42e492",
+    "url": "/robofriends/static/js/2.b362a893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.b362a893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3211c322366f40e90431",
+    "url": "/robofriends/static/js/main.2e17cdde.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/robofriends/static/media/spinner.235b9b3e.gif"
+  }
+]);
